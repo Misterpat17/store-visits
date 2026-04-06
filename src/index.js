9dea8640a9163771fb284474b17833a3c4456d3b
Misterpat17 <pataniadaniele@hotmail.it>
2026-04-06 23:40:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Disregistra qualsiasi service worker precedente
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+      console.log('Service worker rimosso');
+    });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
